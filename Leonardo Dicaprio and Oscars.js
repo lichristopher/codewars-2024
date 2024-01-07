381b59fc19278f2c6d16ad57e213d2a0c9afe8fe
Christopher Li <li.christopher.jr@gmail.com>
2024-01-07 21:53:00 +0800

    add solution link

---
+++ Leonardo Dicaprio and Oscars.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/56d49587df52101de70011e4/solutions
+const leo = (oscar) => {
+  return oscar === 88
+    ? 'Leo finally won the oscar! Leo is happy'
+    : oscar === 86
+    ? 'Not even for Wolf of wallstreet?!'
+    : oscar < 88
+    ? 'When will you give Leo an Oscar?'
+    : 'Leo got one already!';
+};
